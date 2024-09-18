aaebff9155ccda2cc6846edd6a29e5adcb763f3e
Daniel Roulin <dadaroulin@bluewin.ch>
2024-09-18 23:32:39 +0200

    L'extraction des élèves depuis la page fonctionne!

--- src/lib/scraping/eleves.ts
+++ src/lib/scraping/eleves.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import puppeteer, { Page } from 'puppeteer';
 import { client } from "./scrape";
 
 const annees = [
@@ -37,37 +38,57 @@ function deleteFile(filePath: string): void {
 export async function scrapeEleves() {
     const data = await client.sql`SELECT * FROM sections`;
     const sections = data.rows.map(section => section.code);
-
-    const state = readFileIfExists(stateFilePath);
-    const lastState = state ? state : null;
+    // const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    
+    // const state = readFileIfExists(stateFilePath);
+    // const lastState = state ? state : null;
     let sectionStart = 0;
     let anneeStart = 0;
-    if (lastState) {
-        sectionStart = sections.indexOf(lastState.section);
-        anneeStart = annees.indexOf(lastState.annee);
+    // if (lastState) {
+    //     sectionStart = sections.indexOf(lastState.section);
+    //     anneeStart = annees.indexOf(lastState.annee);
 
-        if (sectionStart >= sections.length && anneeStart >= annees.length) {
-            console.log('All students have already been scraped!');
-            return;
-        }
+    //     if (sectionStart >= sections.length && anneeStart >= annees.length) {
+    //         console.log('All students have already been scraped!');
+    //         return;
+    //     }
 
-        console.log(`Recovering from ${sectionStart}-${anneeStart}`);
-        await client.sql`ROLLBACK`;
-    }
+    //     console.log(`Recovering from ${sectionStart}-${anneeStart}`);
+    //     await client.sql`ROLLBACK`;
+    // }
 
     for (let sectionIndex = sectionStart; sectionIndex < sections.length; sectionIndex++) {
         for (let anneeIndex = anneeStart; anneeIndex < annees.length; anneeIndex++) {
-            await client.sql`BEGIN`;
-
             const currentSection = sections[sectionIndex];
             const currentAnnee = annees[anneeIndex];
-
-            scrapeVole(currentSection, currentAnnee); // await new Promise(resolve => setTimeout(resolve, 1000));
-
-            await client.sql`COMMIT`;
-            writeFile(stateFilePath, { section: currentSection, annee: currentAnnee });
+            scrapeVole(page, currentSection, currentAnnee); // await new Promise(resolve => setTimeout(resolve, 1000));
+            return
+            // writeFile(stateFilePath, { section: currentSection, annee: currentAnnee });
         }
     }
-    deleteFile(stateFilePath);
+    // deleteFile(stateFilePath);
     console.log('Finished scraping all students!');
+}
+
+async function scrapeVole(page: Page, section: any, annee: string) {
+    // await client.sql`BEGIN`;
+    await page.goto('https://search.epfl.ch/?filter=unit&q=mt-ba3');
+
+    await page.waitForSelector('.table.result__table');
+
+    // Scrape the student names and links
+    const students = await page.$$eval('.result__table tbody tr', rows => {
+      return rows.map(row => {
+        const anchor = row.querySelector('td a');
+        const name = anchor?.textContent?.trim();
+        const link = anchor?.getAttribute('href');
+        return { name, link };
+      });
+    });
+  
+    console.log(students.length);
+
+    // await client.sql`COMMIT`;
 }
