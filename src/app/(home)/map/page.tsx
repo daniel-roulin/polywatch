5850b6ff807144ae245cd57a56d456932fc2cbb3
Daniel Roulin <daniel.roulin@epfl.ch>
2025-02-08 20:06:48 +0100

    Moved stuff round

--- src/app/(home)/map/page.tsx
+++ src/app/(home)/map/page.tsx
@@ -1,16 +1,16 @@
 // import Image from "next/image";
-import DefaultMap from "@/ui/maps/default_map";
-import IframeMap from "@/ui/maps/iframe_map";
-import LeafletMap from "@/ui/maps/leaflet_map";
-import WMTSMap from "@/ui/maps/OL_WMTS_map";
-import XYZMap from "@/ui/maps/OL_XYZ_map";
+import DefaultMap from "@/ui/plan/default_map";
+import IframeMap from "@/ui/plan/iframe_map";
+import LeafletMap from "@/ui/plan/leaflet_map";
+import WMTSMap from "@/ui/plan/OL_WMTS_map";
+import XYZMap from "@/ui/plan/OL_XYZ_map";
 import HelloWorld from "@/ui/component";
 import dynamic from "next/dynamic";
 
 import React from "react";
 
 export default function Home() {
-  const MapComponent = dynamic(() => import('@/ui/maps/leaflet_map'), { ssr: false });
+  const MapComponent = dynamic(() => import('@/ui/plan/leaflet_map'), { ssr: false });
 
   return (
     <main>
